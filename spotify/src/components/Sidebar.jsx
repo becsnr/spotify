ff3f89dbaac74f253eb383b2b8577305a52ce85a
Beca <234265987+becsnr@users.noreply.github.com>
2026-03-24 14:49:32 -0300

    sidebar icons

--- spotify/src/components/Sidebar.jsx
+++ spotify/src/components/Sidebar.jsx
@@ -4,10 +4,23 @@
 // links de playlists
 import styles from './Sidebar.module.css'
 
+import { useState } from 'react'
+
+import { FaBars, FaStream, FaPlus} from "react-icons/fa"
+
 function Sidebar() {
+    const [hover, setHover] = useState(false);
+
     return (
         <sidebar>
-            <p>barra lateral</p>
+            <div className={styles.biblioteca} 
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}>
+                {hover ? <FaStream /> : <FaBars />}
+            </div>
+            <div className={styles.add}>
+                <FaPlus />
+            </div>
         </sidebar>
     )
 }
