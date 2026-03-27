aedfdd85ca1ddd542dba9d9998b1d4422d61d533
Beca <234265987+becsnr@users.noreply.github.com>
2026-03-27 14:58:41 -0300

    navbar css

--- spotify/src/components/Navbar.jsx
+++ spotify/src/components/Navbar.jsx
@@ -4,35 +4,32 @@
 // perfil
 import styles from './Navbar.module.css'
 
-import { FaEllipsisH, FaChevronLeft, FaChevronRight, FaHome, FaSearch, FaBell } from "react-icons/fa"
+import { FaSpotify, FaHome, FaSearch, FaBell } from "react-icons/fa"
 
 function Navbar() {
     return (
         <nav>
-            <div className={styles.pontos}>
-                <FaEllipsisH />
+            <div className={styles.logo}>
+                <FaSpotify />
             </div>
 
-            <div className={styles.direction}>
-                <FaChevronLeft />
-                <FaChevronRight />
+            <div className={styles.center}>
+                <div className={styles.home}>
+                    <FaHome />
+                </div>
+                <div className={styles.search}>
+                    <FaSearch />
+                    <input type="text" placeholder='O que você quer escutar?' />
+                </div>
             </div>
 
-            <div className={styles.home}>
-                <FaHome />
-            </div>
-
-            <div className={styles.search}>
-                <FaSearch />
-                <input type="text" placeholder='O que você quer escutar?' />
-            </div>
-
-            <div className={styles.sino}>
-                <FaBell />
-            </div>
-
-            <div className={styles.perfil}>
-                R
+            <div className={styles.right}>
+                <div className={styles.sino}>
+                    <FaBell />
+                </div>
+                <div className={styles.perfil}>
+                    R
+                </div>
             </div>
         </nav>
     )
