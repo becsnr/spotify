d1839c3b5f785834ad1085a8fb7c88c22023459b
Beca <234265987+becsnr@users.noreply.github.com>
2026-04-08 13:08:43 -0300

    enumerar

--- spotify/src/components/Musicas.jsx
+++ spotify/src/components/Musicas.jsx
@@ -4,11 +4,13 @@ function Musicas( { item, customClass } ) {
     return (
         <>
             <div className={styles.mscs}>
-                {item.map(item => (
+                {item.map((item, index) => (
                     <div key={item.id} className={`${styles[customClass]}`}>
-                        {/* {CONDIÇÃO PRA ARTISTA ? PLAYLIST. USAR USESTATE} */}
-                        <h2>{item.name}</h2>
-                        <p>{item.cantor}</p>
+                        <span>{index + 1}</span>
+                        <div className={styles.info}>
+                            <h2>{item.name}</h2>
+                            <p>{item.cantor}</p>
+                        </div>
                     </div>
                 ))}
             </div>
