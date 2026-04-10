130d239ab49932294d287d45778380b52bbd54ba
Beca <234265987+becsnr@users.noreply.github.com>
2026-04-10 09:13:49 -0300

    musicas dos artistas

--- spotify/src/data/musicas.js
+++ spotify/src/data/musicas.js
@@ -150,5 +150,333 @@ export const musicas = [
         artistaId: 2,
         album: "Midnights",
         time: "3:21",
+    },
+    {
+        id: 20,
+        name: "Fortnight (feat. Post Malone)",
+        cantor: "Taylor Swift",
+        artistaId: 2,
+        album: "The Tortured Poets Department",
+        time: "3:49",
+    },
+    {
+        id: 21,
+        name: "bad",
+        cantor: "Wave to Earth",
+        artistaId: 3,
+        album: "0.1 flaws and all.",
+        time: "4:23"
+    },
+    {
+        id: 22,
+        name: "love.",
+        cantor: "Wave to Earth",
+        artistaId: 3,
+        album: "0.1 flaws and all.",
+        time: "5:08"
+    },
+    {
+        id: 23,
+        name: "peach eyes",
+        cantor: "Wave to Earth",
+        artistaId: 3,
+        album: "0.1 flaws and all.",
+        time: "3:05"
+    },
+    {
+        id: 24,
+        name: "wave",
+        cantor: "Wave to Earth",
+        artistaId: 3,
+        album: "wave",
+        time: "4:30"
+    },
+    {
+        id: 25,
+        name: "sunny days",
+        cantor: "Wave to Earth",
+        artistaId: 3,
+        album: "0.1 flaws and all.",
+        time: "4:08"
+    },
+    {
+        id: 26,
+        name: "calla",
+        cantor: "Wave to Earth",
+        artistaId: 3,
+        album: "0.1 flaws and all.",
+        time: "4:19"
+    },
+    {
+        id: 27,
+        name: "seasons",
+        cantor: "Wave to Earth",
+        artistaId: 3,
+        album: "summer flows 0.02",
+        time: "4:16"
+    },
+    {
+        id: 28,
+        name: "light",
+        cantor: "Wave to Earth",
+        artistaId: 3,
+        album: "light",
+        time: "3:46"
+    },
+    {
+        id: 29,
+        name: "homesick",
+        cantor: "Wave to Earth",
+        artistaId: 3,
+        album: "0.1 flaws and all.",
+        time: "5:06"
+    },
+    {
+        id: 30,
+        name: "daisy.",
+        cantor: "Wave to Earth",
+        artistaId: 3,
+        album: "daisy.",
+        time: "3:39"
+    },
+    {
+        id: 31,
+        name: "From The Start",
+        cantor: "Laufey",
+        artistaId: 4,
+        album: "From The Start",
+        time: "2:50"
+    },
+    {
+        id: 32,
+        name: "Valentine",
+        cantor: "Laufey",
+        artistaId: 4,
+        album: "Everything I Know About Love",
+        time: "2:49"
+    },
+    {
+        id: 33,
+        name: "Like the Movies",
+        cantor: "Laufey",
+        artistaId: 4,
+        album: "Typical of Me EP",
+        time: "2:43"
+    },
+    {
+        id: 34,
+        name: "Falling Behind",
+        cantor: "Laufey",
+        artistaId: 4,
+        album: "Everything I Know About Love",
+        time: "2:54"
+    },
+    {
+        id: 35,
+        name: "Bewitched",
+        cantor: "Laufey",
+        artistaId: 4,
+        album: "Bewitched",
+        time: "4:07"
+    },
+    {
+        id: 36,
+        name: "Fragile",
+        cantor: "Laufey",
+        artistaId: 4,
+        album: "Everything I Know About Love",
+        time: "4:01"
+    },
+    {
+        id: 37,
+        name: "I Wish You Love",
+        cantor: "Laufey",
+        artistaId: 4,
+        album: "Typical of Me EP",
+        time: "2:35"
+    },
+    {
+        id: 38,
+        name: "Slow Down",
+        cantor: "Laufey",
+        artistaId: 4,
+        album: "Everything I Know About Love",
+        time: "2:25"
+    },
+    {
+        id: 39,
+        name: "Dreamer",
+        cantor: "Laufey",
+        artistaId: 4,
+        album: "Bewitched",
+        time: "3:31"
+    },
+    {
+        id: 40,
+        name: "Lover Girl",
+        cantor: "Laufey",
+        artistaId: 4,
+        album: "A Matter of Time",
+        time: "2:44"
+    },
+    {
+        id: 41,
+        name: "eyes on Him",
+        cantor: "Strings & Heart",
+        artistaId: 5,
+        album: "eyes on Him",
+        time: "3:12"
+    },
+    {
+        id: 42,
+        name: "evergreen love",
+        cantor: "Strings & Heart",
+        artistaId: 5,
+        album: "Plastic Wine",
+        time: "2:58"
+    },
+    {
+        id: 43,
+        name: "rescue",
+        cantor: "Strings & Heart",
+        artistaId: 5,
+        album: "Secret Place",
+        time: "3:06"
+    },
+    {
+        id: 44,
+        name: "more of you",
+        cantor: "Strings & Heart",
+        artistaId: 5,
+        album: "Secret Place",
+        time: "3:27"
+    },
+    {
+        id: 45,
+        name: "sunset silhouette",
+        cantor: "Strings & Heart",
+        artistaId: 5,
+        album: "Plastic Wine",
+        time: "2:56"
+    },
+    {
+        id: 46,
+        name: "love you to death",
+        cantor: "Strings & Heart",
+        artistaId: 5,
+        album: "The Prodigal (Deluxe Edition)",
+        time: "2:52"
+    },
+    {
+        id: 47,
+        name: "honeydew (praise the Lord)",
+        cantor: "Strings & Heart",
+        artistaId: 5,
+        album: "Plastic Wine",
+        time: "3:44"
+    },
+    {
+        id: 48,
+        name: "kerose",
+        cantor: "Strings & Heart",
+        artistaId: 5,
+        album: "Plastic Wine",
+        time: "2:51"
+    },
+    {
+        id: 49,
+        name: "dopamine",
+        cantor: "Strings & Heart",
+        artistaId: 5,
+        album: "Plastic Wine",
+        time: "2:02"
+    },
+    {
+        id: 50,
+        name: "oasis for my soul",
+        cantor: "Strings & Heart",
+        artistaId: 5,
+        album: "Plastic Wine",
+        time: "2:16"
+    },
+    {
+        id: 51,
+        name: "Hype Boy",
+        cantor: "NewJeans",
+        artistaId: 6,
+        album: "NewJeans 1st EP'New Jeans'",
+        time: "2:59"
+    },
+    {
+        id: 52,
+        name: "Attention",
+        cantor: "NewJeans",
+        artistaId: 6,
+        album: "NewJeans 1st EP'New Jeans'",
+        time: "3:00"
+    },
+    {
+        id: 53,
+        name: "Cookie",
+        cantor: "NewJeans",
+        artistaId: 6,
+        album: "NewJeans 1st EP'New Jeans'",
+        time: "3:56"
+    },
+    {
+        id: 54,
+        name: "Hurt",
+        cantor: "NewJeans",
+        artistaId: 6,
+        album: "NewJeans 1st EP'New Jeans'",
+        time: "2:58"
+    },
+    {
+        id: 55,
+        name: "OMG",
+        cantor: "NewJeans",
+        artistaId: 6,
+        album: "NewJeans'OMG'",
+        time: "3:32"
+    },
+    {
+        id: 56,
+        name: "Ditto",
+        cantor: "NewJeans",
+        artistaId: 6,
+        album: "NewJeans'OMG'",
+        time: "3:05"
+    },
+    {
+        id: 57,
+        name: "Super Shy",
+        cantor: "NewJeans",
+        artistaId: 6,
+        album: "NewJeans 2nd EP'Get Up'",
+        time: "2:34"
+    },
+    {
+        id: 58,
+        name: "Cool With You",
+        cantor: "NewJeans",
+        artistaId: 6,
+        album: "NewJeans 2nd EP'Get Up'",
+        time: "2:27"
+    },
+    {
+        id: 59,
+        name: "ETA",
+        cantor: "NewJeans",
+        artistaId: 6,
+        album: "NewJeans 2nd EP'Get Up'",
+        time: "2:31"
+    },
+    {
+        id: 60,
+        name: "ASAP",
+        cantor: "NewJeans",
+        artistaId: 6,
+        album: "NewJeans 2nd EP'Get Up'",
+        time: "2:14"
     }
 ]
