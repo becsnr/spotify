55c2337e4655076f46192358a057c25ff3fa22c5
Beca <234265987+becsnr@users.noreply.github.com>
2026-04-12 07:50:07 -0300

    header

--- spotify/src/components/HeaderPlaylist.jsx
+++ spotify/src/components/HeaderPlaylist.jsx
@@ -1,15 +1,15 @@
 // PLAYLIST E ALBUM
-// import styles from './.module.css'
+import styles from './HeaderPlaylist.module.css'
 
 function HeaderPlaylist({ imagem, titulo, subtitulo, descricao }) {
     return (
-        <div >
-            <img src={imagem} />
+        <div className={styles.header}>
+            <img src={imagem} className={styles.capa} />
 
-            <div>
-                <p>{subtitulo}</p>
-                <h1>{titulo}</h1>
-                <p>{descricao}</p>
+            <div className={styles.info}>
+                <p className={styles.subtitulo}>{subtitulo}</p>
+                <h1 className={styles.titulo}>{titulo}</h1>
+                <p className={styles.descricao}>{descricao}</p>
             </div>
         </div>
     )
