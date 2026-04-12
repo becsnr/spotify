ca10dd432022e18473e18577fd6deb7f9da2ca21
Beca <234265987+becsnr@users.noreply.github.com>
2026-04-12 18:47:06 -0300

    gradiente

--- spotify/src/components/HeaderPlaylist.jsx
+++ spotify/src/components/HeaderPlaylist.jsx
@@ -1,9 +1,28 @@
 // PLAYLIST E ALBUM
+import { useEffect, useState } from 'react'
+import {Vibrant} from "node-vibrant/browser"
+
 import styles from './HeaderPlaylist.module.css'
 
 function HeaderPlaylist({ imagem, titulo, subtitulo, descricao }) {
+    // pra pegar as cores das capas
+    const [color, setColor] = useState("#000")
+
+    useEffect( () => {
+        if (!imagem) return
+
+        Vibrant.from(imagem).getPalette().then(palette => {
+            const cor = palette.Vibrant?.hex || palette.DarkVibrant?.hex || "#121212"
+            setColor(cor)
+        })
+        .catch(err => {
+            console.log("Erro Vibrant: ", err)
+            setColor("#121212")
+        })
+    }, [imagem])
+
     return (
-        <div className={styles.header}>
+        <div className={styles.header} style={{background:`linear-gradient(${color}, #121212)`}}>
             <img src={imagem} className={styles.capa} />
 
             <div className={styles.info}>
