47e0bffbf922ed59c9883233d7b52a8c6a2cbb96
Beca <234265987+becsnr@users.noreply.github.com>
2026-04-15 13:54:41 -0300

    albuns

--- spotify/src/data/musicas.js
+++ spotify/src/data/musicas.js
@@ -586,5 +586,491 @@ export const musicas = [
         album: "das coisas que carrego dentro",
         albumId: 1,
         time: "3:50"
+    },
+    {
+        id: 73,
+        name: "The Girl From Ipanema",
+        cantor: "Stan Getz",
+        artistaId: 8,
+        album: "Getz/Gilberto (Expanded Edition)",
+        albumId: 2,
+        time: "5:20"
+    },
+    {
+        id: 74,
+        name: "Doralice - Stereo Version",
+        cantor: "Stan Getz",
+        artistaId: 8,
+        album: "Getz/Gilberto (Expanded Edition)",
+        albumId: 2,
+        time: "2:46"
+    },
+    {
+        id: 74,
+        name: "Para Machucar Meu Coração",
+        cantor: "Stan Getz",
+        artistaId: 8,
+        album: "Getz/Gilberto (Expanded Edition)",
+        albumId: 2,
+        time: "5:07"
+    },
+    {
+        id: 75,
+        name: "Desafinado",
+        cantor: "Stan Getz",
+        artistaId: 8,
+        album: "Getz/Gilberto (Expanded Edition)",
+        albumId: 2,
+        time: "4:08"
+    },
+    {
+        id: 76,
+        name: "Corcovado (Quiet Nights Of Quiet Stars)",
+        cantor: "Stan Getz",
+        artistaId: 8,
+        album: "Getz/Gilberto (Expanded Edition)",
+        albumId: 2,
+        time: "4:16"
+    },
+    {
+        id: 77,
+        name: "Só Danço Samba - Stereo Version",
+        cantor: "Stan Getz",
+        artistaId: 8,
+        album: "Getz/Gilberto (Expanded Edition)",
+        albumId: 2,
+        time: "3:40"
+    },
+    {
+        id: 78,
+        name: "O Grande Amor",
+        cantor: "Stan Getz",
+        artistaId: 8,
+        album: "Getz/Gilberto (Expanded Edition)",
+        albumId: 2,
+        time: "5:27"
+    },
+    {
+        id: 79,
+        name: "Vivo Sohando - Stereo Version",
+        cantor: "Stan Getz",
+        artistaId: 8,
+        album: "Getz/Gilberto (Expanded Edition)",
+        albumId: 2,
+        time: "2:56"
+    },
+    {
+        id: 80,
+        name: "Meia Noite - Ao Vivo",
+        cantor: "fhop music",
+        artistaId: 9,
+        album: "Meia Noite (Ao Vivo)",
+        albumId: 3,
+        time: "3:43"
+    },
+    {
+        id: 81,
+        name: "Quebra o Silêncio - Ao Vivo",
+        cantor: "fhop music",
+        artistaId: 9,
+        album: "Meia Noite (Ao Vivo)",
+        albumId: 3,
+        time: "5:58"
+    },
+    {
+        id: 82,
+        name: "Há Poder - Ao Vivo",
+        cantor: "fhop music",
+        artistaId: 9,
+        album: "Meia Noite (Ao Vivo)",
+        albumId: 3,
+        time: "7:16"
+    },
+    {
+        id: 83,
+        name: "Dono da Minha Afeição",
+        cantor: "fhop music",
+        artistaId: 9,
+        album: "Meia Noite (Ao Vivo)",
+        albumId: 3,
+        time: "5:58"
+    },
+    {
+        id: 84,
+        name: "Sublime - Ao Vivo",
+        cantor: "fhop music",
+        artistaId: 9,
+        album: "Meia Noite (Ao Vivo)",
+        albumId: 3,
+        time: "7:25"
+    },
+    {
+        id: 85,
+        name: "Fé - Ao Vivo",
+        cantor: "fhop music",
+        artistaId: 9,
+        album: "Meia Noite (Ao Vivo)",
+        albumId: 3,
+        time: "3:45"
+    },
+    {
+        id: 86,
+        name: "Indescupável - Ao Vivo",
+        cantor: "fhop music",
+        artistaId: 9,
+        album: "Meia Noite (Ao Vivo)",
+        albumId: 3,
+        time: "4:52"
+    },
+    {
+        id: 87,
+        name: "Majestade - Ao Vivo",
+        cantor: "fhop music",
+        artistaId: 9,
+        album: "Meia Noite (Ao Vivo)",
+        albumId: 3,
+        time: "6:01"
+    },
+    {
+        id: 88,
+        name: "Alfa e Ômega - Ao Vivo",
+        cantor: "fhop music",
+        artistaId: 9,
+        album: "Meia Noite (Ao Vivo)",
+        albumId: 3,
+        time: "1:24"
+    },
+    {
+        id: 89,
+        name: "Suficiente + Doxologia - Ao Vivo",
+        cantor: "fhop music",
+        artistaId: 9,
+        album: "Meia Noite (Ao Vivo)",
+        albumId: 3,
+        time: "5:11"
+    },
+    {
+        id: 90,
+        name: "Obediência - Ao Vivo",
+        cantor: "fhop music",
+        artistaId: 9,
+        album: "Meia Noite (Ao Vivo)",
+        albumId: 3,
+        time: "5:06"
+    },
+    {
+        id: 91,
+        name: "Tudo Novo de Novo (Intro)",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "3:07"
+    },
+    {
+        id: 92,
+        name: "Publicano",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "3:13"
+    },
+    {
+        id: 93,
+        name: "Sobre Ele",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "3:36"
+    },
+    {
+        id: 94,
+        name: "Religare",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "3:20"
+    },
+    {
+        id: 95,
+        name: "Bereianos",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "4:37"
+    },
+    {
+        id: 96,
+        name: "Flow João Batista",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "3:24"
+    },
+    {
+        id: 97,
+        name: "Mortis",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "4:01"
+    },
+    {
+        id: 98,
+        name: "Sangue Carmesim",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "2:51"
+    },
+    {
+        id: 99,
+        name: "Pão & Vinho",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "3:37"
+    },
+    {
+        id: 100,
+        name: "Pedro",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "3:21"
+    },
+    {
+        id: 101,
+        name: "O Plano",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "3:37"
+    },
+    {
+        id: 102,
+        name: "Teorema de Jó",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "2:46"
+    },
+    {
+        id: 103,
+        name: "Brisa",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "4:06"
+    },
+    {
+        id: 104,
+        name: "Ventre",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "3:41"
+    },
+    {
+        id: 105,
+        name: "Desde a Eternidade",
+        cantor: "Kami Kam",
+        artistaId: 10,
+        album: "Post Tenebras Lux",
+        albumId: 4,
+        time: "3:48"
+    },
+    {
+        id: 106,
+        name: "Não Há Amor Igual - Ao Vivo",
+        cantor: "Morada",
+        artistaId: 11,
+        album: "Uma Só Voz - Vol.01 (Ao Vivo)",
+        albumId: 5,
+        time: "5:52"
+    },
+    {
+        id: 107,
+        name: "Emaús - Ao Vivo",
+        cantor: "Morada",
+        artistaId: 11,
+        album: "Uma Só Voz - Vol.01 (Ao Vivo)",
+        albumId: 5,
+        time: "6:58"
+    },
+    {
+        id: 108,
+        name: "Isaías 6 - Ao Vivo",
+        cantor: "Morada",
+        artistaId: 11,
+        album: "Uma Só Voz - Vol.01 (Ao Vivo)",
+        albumId: 5,
+        time: "7:09"
+    },
+    {
+        id: 109,
+        name: "Intimidade - Ao Vivo",
+        cantor: "Morada",
+        artistaId: 11,
+        album: "Uma Só Voz - Vol.01 (Ao Vivo)",
+        albumId: 5,
+        time: "3:39"
+    },
+    {
+        id: 110,
+        name: "Ele Me Amou Primeiro - Ao Vivo",
+        cantor: "Morada",
+        artistaId: 11,
+        album: "Uma Só Voz - Vol.01 (Ao Vivo)",
+        albumId: 5,
+        time: "4:56"
+    },
+    {
+        id: 111,
+        name: "Postura do Reino - Ao Vivo",
+        cantor: "Morada",
+        artistaId: 11,
+        album: "Uma Só Voz - Vol.01 (Ao Vivo)",
+        albumId: 5,
+        time: "5:43"
+    },
+    {
+        id: 112,
+        name: "Meus Votos / Dia e Noite - Ao Vivo",
+        cantor: "Morada",
+        artistaId: 11,
+        album: "Uma Só Voz - Vol.01 (Ao Vivo)",
+        albumId: 5,
+        time: "7:11"
+    },
+    {
+        id: 113,
+        name: "Só Tu És Santo / Uma Coisa / Deixa Queimar / Quando Ele Vem - Ao Vivo",
+        cantor: "Morada",
+        artistaId: 11,
+        album: "Uma Só Voz - Vol.01 (Ao Vivo)",
+        albumId: 5,
+        time: "8:46"
+    },
+    {
+        id: 114,
+        name: "É Tudo Sobre Você / Ser Mudado - Ao Vivo",
+        cantor: "Morada",
+        artistaId: 11,
+        album: "Uma Só Voz - Vol.01 (Ao Vivo)",
+        albumId: 5,
+        time: "7:22"
+    },
+    {
+        id: 115,
+        name: "Bendirei / Como Quiser - Ao Vivo",
+        cantor: "Morada",
+        artistaId: 11,
+        album: "Uma Só Voz - Vol.01 (Ao Vivo)",
+        albumId: 5,
+        time: "7:32"
+    },
+    {
+        id: 116,
+        name: "Savior (feat. B.I)",
+        cantor: "Leehi",
+        artistaId: 12,
+        album: "4 Only",
+        albumId: 6,
+        time: "3:35"
+    },
+    {
+        id: 117,
+        name: "Intentions",
+        cantor: "Leehi",
+        artistaId: 12,
+        album: "4 Only",
+        albumId: 6,
+        time: "3:24"
+    },
+    {
+        id: 118,
+        name: "Waterride",
+        cantor: "Leehi",
+        artistaId: 12,
+        album: "4 Only",
+        albumId: 6,
+        time: "3:04"
+    },
+    {
+        id: 119,
+        name: "Bye",
+        cantor: "Leehi",
+        artistaId: 12,
+        album: "4 Only",
+        albumId: 6,
+        time: "3:10"
+    },
+    {
+        id: 120,
+        name: "Red Lipstick (feat. Yoonmirae)",
+        cantor: "Leehi",
+        artistaId: 12,
+        album: "4 Only",
+        albumId: 6,
+        time: "3:07"
+    },
+    {
+        id: 121,
+        name: "H.S.K.T. (feat. Wonstein)",
+        cantor: "Leehi",
+        artistaId: 12,
+        album: "4 Only",
+        albumId: 6,
+        time: "3:24"
+    },
+    {
+        id: 122,
+        name: "Safety Zone",
+        cantor: "Leehi",
+        artistaId: 12,
+        album: "4 Only",
+        albumId: 6,
+        time: "4:08"
+    },
+    {
+        id: 123,
+        name: "What is Love?",
+        cantor: "Leehi",
+        artistaId: 12,
+        album: "4 Only",
+        albumId: 6,
+        time: "3:43"
+    },
+    {
+        id: 124,
+        name: "Darling",
+        cantor: "Leehi",
+        artistaId: 12,
+        album: "4 Only",
+        albumId: 6,
+        time: "3:55"
+    },
+    {
+        id: 125,
+        name: "Only",
+        cantor: "Leehi",
+        artistaId: 12,
+        album: "4 Only",
+        albumId: 6,
+        time: "4:00"
     }
 ]
