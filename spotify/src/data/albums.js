85468c0a635baab4b90b89daa596c7320f6e46cf
Beca <234265987+becsnr@users.noreply.github.com>
2026-04-15 10:48:44 -0300

    update albuns

--- spotify/src/data/albums.js
+++ spotify/src/data/albums.js
@@ -2,14 +2,19 @@ import josejr from "../assets/albuns/josejr-album.jpg"
 import josePerfil from "../assets/artistas/josejr-card.jpg"
 
 import getz from "../assets/albuns/getz-album.jpg"
+import stanGetz from "../assets/artistas/stan_getz.jpg"
 
-import fhopeCapa from "../assets/albuns/fhope-album.jpg"
+import fhopCapa from "../assets/albuns/fhope-album.jpg"
+import fhop from "../assets/artistas/fhop.jpg"
 
 import kami from "../assets/albuns/kami-album.jpg"
+import kamiKam from "../assets/artistas/kami_kam.jpg"
 
 import morada from "../assets/albuns/morada-album.jpg"
+import moradaBand from "../assets/artistas/morada.jpg"
 
 import only from "../assets/albuns/only-album.jpg"
+import leehi from "../assets/artistas/leehi.jpg"
 
 export const albums = [
     {
@@ -23,41 +28,41 @@ export const albums = [
     {
         id: 2,
         name: "Getz/Gilberto (Expanded Edition)",
-        // ano: ,
+        ano: 1964,
         imgCard: getz,
         artista: "Stan Getz & João Gilberto",
-        // imgPerfil: 
+        imgPerfil: stanGetz
     },
     {
         id: 3,
         name: "Meia Noite (Ao Vivo)",
-        // ano: ,
-        imgCard: fhopeCapa,
-        artista: "FHOPE",
-        // imgPerfil: 
+        ano: 2025,
+        imgCard: fhopCapa,
+        artista: "fhop music",
+        imgPerfil: fhop
     },
     {
         id: 4,
         name: "Post Tenebras Lux",
-        // ano: ,
+        ano: 2021,
         imgCard: kami,
         artista: "Kami Kam",
-        // imgPerfil: 
+        imgPerfil: kamiKam
     },
     {
         id: 5,
         name: "Uma Só Voz - Vol. 01 (Ao Vivo)",
-        // ano: ,
+        ano: 2021,
         imgCard: morada,
         artista: "Morada",
-        // imgPerfil: 
+        imgPerfil: moradaBand
     },
     {
         id: 6,
-        name: "Only",
-        // ano: ,
+        name: "4 Only",
+        ano: 2021,
         imgCard: only,
         artista: "Leehi",
-        // imgPerfil: 
+        imgPerfil: leehi
     }
 ]
